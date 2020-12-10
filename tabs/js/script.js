38d000637d9890b44bf80cb1e93ed3df824ca53e
yemets <yemetsvova@gmail.com>
2020-12-10 16:51:14 +0200

    modal window

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -91,18 +91,32 @@ window.addEventListener('DOMContentLoaded', () => {
     setClock('.timer', deadline);
 
     //Modal
-
     const modalTrigger = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('.modal');
-    const modalCloseBtn = querySelector('[data-close]');
+    const modalCloseBtn = document.querySelector('[data-close]');
 
-    modalTrigger.addEventListener('click', () => {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
     });
+    function closeModal(){
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
 
-    modalCloseBtn.addEventListener('click', () => {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
+    modalCloseBtn.addEventListener('click', closeModal);
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
+        }
     });
 });
