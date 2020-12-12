dbf9bf7e2bb5dbc79a43b61810ff462e065d3854
yemets <yemetsvova@gmail.com>
2020-12-12 21:57:45 +0200

    Fetch API

--- tabs/js/script.js
+++ tabs/js/script.js
@@ -230,13 +230,9 @@ window.addEventListener('DOMContentLoaded', () => {
             display: block;
             margin: 0 auto;
             `;
-            // form.append(statusMessage);
+            form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'aplication/json');
             const formData = new FormData(form);
 
             const object = {};
@@ -244,21 +240,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
+            fetch('server1.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'aplication/json'
+                },
+                body: JSON.stringify(object)
 
-            // request.send(formData);
-            request.send(json);
+            }).then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                  
-                } else {
-                    showThanksModal(message.failure);
-                }
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
             });
 
         });
@@ -289,5 +287,5 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
-
+    
 });
